a0990faa52ac70b363a27f13cc42eed610d257e5
vanfigo <rodrigo.loyola@live.com>
2019-09-06 19:04:53 -0500

    first commit

---
+++ src/main/java/com/bucketdev/betapppoolsvc/BetappPoolSvcApplication.java
@@ -0,0 +1,13 @@
+package com.bucketdev.betapppoolsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappPoolSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetappPoolSvcApplication.class, args);
+	}
+
+}
